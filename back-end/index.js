ca3a61e90026b0b658416571e3bd9ae0b3880633
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-07-30 22:56:56 +0300

    Added nodemon

--- back-end/index.js
+++ back-end/index.js
@@ -2,14 +2,23 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+  res.json({
+    success: true,
+  });
+});
+
 app.listen(process.env.PORT || 5555, (error) => {
-  if(error) {
+  if (error) {
     return console.log(error);
   }
 
-  console.log('Server has been started.')
-})
+  console.log("Server has been started.");
+});
