e6e1695185744f33f4227fb5e5f4b524d3990ee5
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-07-31 22:29:07 +0300

    Created CRUD Post Controllers

--- back-end/index.js
+++ back-end/index.js
@@ -2,11 +2,16 @@ import express from "express";
 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import { registerValidation, loginValidation } from "./validation.js";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validation.js";
 
 import checkAuth from "./utils/checkAuth.js";
 
 import * as UseController from "./controllers/UserController.js";
+import * as PostController from "./controllers/PostController.js";
 
 dotenv.config();
 
@@ -26,6 +31,12 @@ app.use(express.json());
 app.post("/auth/register", registerValidation, UseController.register);
 app.post("/auth/login", loginValidation, UseController.login);
 app.get("/auth/me", checkAuth, UseController.getMe);
+
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch("/posts/:id", checkAuth, PostController.update);
 
 app.listen(process.env.PORT || 5555, (error) => {
   if (error) {
