2b8b324b20512d5eb9f29a68a3fae056521ae69b
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-07-31 14:00:02 +0300

    Connected DB || Created User Model || Added express-validator, mongoose, dotenv, argon2

--- back-end/index.js
+++ back-end/index.js
@@ -1,33 +1,73 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import argon2 from "argon2";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { registerValidation } from "./validations/auth.js";
+import { validationResult } from "express-validator";
+import UserModel from "./models/User.js";
+
+dotenv.config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.w9q0fbx.mongodb.net/blog?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("DB is OK");
+  })
+  .catch((error) => console.log("Db Error", error));
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
 
-app.post("/auth/login", (req, res) => {
-  const token = jwt.sign(
-    {
+    const password = req.body.password;
+    const hash = await argon2.hash(password, { type: argon2.argon2id });
+
+    const doc = new UserModel({
       email: req.body.email,
-      fullname: "Anton",
-    },
-    "secretkey"
-  );
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
 
-  res.json({
-    success: true,
-    token,
-  });
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      process.env.JWT_KEY,
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...useData } = user._doc;
+
+    res.json({
+      ...useData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Registration failed",
+    });
+  }
 });
 
 app.listen(process.env.PORT || 5555, (error) => {
   if (error) {
     return console.log(error);
   }
-
   console.log("Server has been started.");
 });
---
+++ back-end/validations/auth.js
@@ -0,0 +1,12 @@
+import { body } from "express-validator";
+
+export const registerValidation = [
+  body("email", "invalid mail format").isEmail(),
+  body("password", "The minimum password length is 5 characters.").isLength({
+    min: 5,
+  }),
+  body("fullName", "The minimum name length is 2 characters.").isLength({
+    min: 2,
+  }),
+  body("avatarUrl", "Invalid link").optional().isURL(),
+];
