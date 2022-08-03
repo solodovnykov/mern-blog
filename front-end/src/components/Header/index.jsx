b164f134eec9aed48f90a5ab7ed7adeac3113c3e
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-08-03 22:01:07 +0300

    Added React-Router

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button";
 
 import styles from "./Header.module.scss";
 import Container from "@mui/material/Container";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   const isAuth = false;
@@ -13,15 +14,15 @@ export const Header = () => {
     <div className={styles.root}>
       <Container maxWidth="lg">
         <div className={styles.inner}>
-          <a className={styles.logo} href="/">
+          <Link className={styles.logo} to="/">
             <div>Mern Blog</div>
-          </a>
+          </Link>
           <div className={styles.buttons}>
             {isAuth ? (
               <>
-                <a href="/posts/create">
+                <Link to="/posts/create">
                   <Button variant="contained">Create post</Button>
-                </a>
+                </Link>
                 <Button
                   onClick={onClickLogout}
                   variant="contained"
@@ -31,12 +32,12 @@ export const Header = () => {
               </>
             ) : (
               <>
-                <a href="/login">
+                <Link to="/login">
                   <Button variant="outlined">Sign In</Button>
-                </a>
-                <a href="/register">
+                </Link>
+                <Link to="/register">
                   <Button variant="contained">Sign Up</Button>
-                </a>
+                </Link>
               </>
             )}
           </div>
