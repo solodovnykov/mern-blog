b90b5c2a2f80f206ba5d567e7e2e6114684fc04b
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-08-06 14:52:42 +0300

    Added Post Page Fetching

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -40,9 +40,9 @@ export const Home = () => {
                 <Post key={index} isLoading={true} />
               ) : (
                 <Post
-                  id={item._id}
+                  _id={item._id}
                   title={item.title}
-                  imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+                  imageUrl={item.imageUrl}
                   user={item.user}
                   createdAt={item.createdAt}
                   viewsCount={item.viewsCount}
