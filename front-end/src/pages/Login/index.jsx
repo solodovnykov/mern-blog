cd6b3d25252579938b8e2f27b17fd0597e7a3dfc
Anton Solodovnykov <solodovnikov.toni@gmail.com>
2022-08-06 17:10:22 +0300

    Added Authorization And Logout

--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -6,46 +6,81 @@ import Button from "@mui/material/Button";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+
 import styles from "./Login.module.scss";
 import { IconButton, InputAdornment } from "@mui/material";
 
+import { useForm } from "react-hook-form";
+import { fetchAuth, selectIsAuth } from "../../redux/slices/auth";
+
 export const Login = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => setShowPassword(!showPassword);
   const handleMouseDownPassword = () => setShowPassword(!showPassword);
+
+  const onSubmit = (values) => {
+    dispatch(fetchAuth(values));
+  };
+
+  if (isAuth) {
+    return <Navigate to="/" />;
+  }
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Sign In
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Invalid email"
-        fullWidth
-      />
-      <TextField
-        className={styles.field}
-        label="Password"
-        fullWidth
-        type={showPassword ? "text" : "password"}
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}>
-                {showPassword ? <Visibility /> : <VisibilityOff />}
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-      />
-      <Button size="large" variant="contained" fullWidth>
-        Sign In
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          type="email"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          fullWidth
+          {...register("email", { required: "Enter email" })}
+        />
+        <TextField
+          className={styles.field}
+          label="Password"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          fullWidth
+          type={showPassword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}>
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          {...register("password", { required: "Enter password" })}
+        />
+        <Button type="submit" size="large" variant="contained" fullWidth>
+          Sign In
+        </Button>
+      </form>
     </Paper>
   );
 };
--- front-end/src/redux/slices/auth.js
+++ front-end/src/redux/slices/auth.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../axios";
 
-export const fetchUserData = createAsyncThunk(
+export const fetchAuth = createAsyncThunk(
   "auth/fetchUserData",
   async (params) => {
     const { data } = await axios.post("/auth/login", params);
@@ -17,20 +17,29 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
+  reducers: {
+    logout: (state) => {
+      state.data = null;
+    },
+  },
   extraReducers: {
-    [fetchUserData.pending]: (state) => {
+    [fetchAuth.pending]: (state) => {
       state.status = "loading";
       state.data = null;
     },
-    [fetchUserData.fulfilled]: (state, action) => {
+    [fetchAuth.fulfilled]: (state, action) => {
       state.status = "loaded";
       state.data = action.payload;
     },
-    [fetchUserData.rejected]: (state) => {
+    [fetchAuth.rejected]: (state) => {
       state.status = "error";
       state.data = null;
     },
   },
 });
 
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
 export const authReducer = authSlice.reducer;
+
+export const { logout } = authSlice.actions;
